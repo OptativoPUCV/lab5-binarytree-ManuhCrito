52c94673e4d11e1ab11cec7ae3a3a97a00bbecee
Your Name <you@example.com>
2024-05-09 22:52:53 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -6,27 +6,31 @@
 typedef struct TreeNode TreeNode;
 
 
-struct TreeNode {
+struct TreeNode
+{
     Pair* pair;
     TreeNode * left;
     TreeNode * right;
     TreeNode * parent;
 };
 
-struct TreeMap {
+struct TreeMap 
+{
     TreeNode * root;
     TreeNode * current;
     int (*lower_than) (void* key1, void* key2);
 };
 
-int is_equal(TreeMap* tree, void* key1, void* key2){
+int is_equal(TreeMap* tree, void* key1, void* key2)
+{
     if(tree->lower_than(key1,key2)==0 &&  
         tree->lower_than(key2,key1)==0) return 1;
     else return 0;
 }
 
 
-TreeNode * createTreeNode(void* key, void * value) {
+TreeNode * createTreeNode(void* key, void * value)
+{
     TreeNode * new = (TreeNode *)malloc(sizeof(TreeNode));
     if (new == NULL) return NULL;
     new->pair = (Pair *)malloc(sizeof(Pair));
@@ -36,28 +40,37 @@ TreeNode * createTreeNode(void* key, void * value) {
     return new;
 }
 
-TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
+TreeMap * createTreeMap(int (*lower_than) (void* keyA, void* keyB))
+{
+    TreeMap * new = (TreeMap *)malloc(sizeof(TreeMap));
+    
+    new->lower_than = lower_than;
+    if (new->lower_than == NULL) return NULL;
 
-    //new->lower_than = lower_than;
+    new->root = NULL;
+    return new;
+}
+
+
+void insertTreeMap(TreeMap * tree, void* key, void * value)
+{
+
+}
+
+TreeNode * minimum(TreeNode * x)
+{
+
     return NULL;
 }
 
 
-void insertTreeMap(TreeMap * tree, void* key, void * value) {
+void removeNode(TreeMap * tree, TreeNode* node)
+{
 
 }
 
-TreeNode * minimum(TreeNode * x){
-
-    return NULL;
-}
-
-
-void removeNode(TreeMap * tree, TreeNode* node) {
-
-}
-
-void eraseTreeMap(TreeMap * tree, void* key){
+void eraseTreeMap(TreeMap * tree, void* key
+{
     if (tree == NULL || tree->root == NULL) return;
 
     if (searchTreeMap(tree, key) == NULL) return;
@@ -69,19 +82,23 @@ void eraseTreeMap(TreeMap * tree, void* key){
 
 
 
-Pair * searchTreeMap(TreeMap * tree, void* key) {
+Pair * searchTreeMap(TreeMap * tree, void* key)
+{
     return NULL;
 }
 
 
-Pair * upperBound(TreeMap * tree, void* key) {
+Pair * upperBound(TreeMap * tree, void* key)
+{
     return NULL;
 }
 
-Pair * firstTreeMap(TreeMap * tree) {
+Pair * firstTreeMap(TreeMap * tree)
+{
     return NULL;
 }
 
-Pair * nextTreeMap(TreeMap * tree) {
+Pair * nextTreeMap(TreeMap * tree)
+{
     return NULL;
 }
